dd392e728c937d7b13af0d228105f664ecb10a60
claranceJ <jacktonec@gmail.com>
2024-09-17 14:56:59 +0300

    Feat: Purged the tailwind config file

--- jepco-limited/tailwind.config.js
+++ jepco-limited/tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1a237e", //Dark blue
+        secondary: "rgb(14, 165, 233)", //Red
+        accent: "ffffff", //white
+
+      },
+      fontFamily: {
+        sans: ['Poppins','sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
